2be92c9a8080256ccc03f675c8cfa3acde74afef
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-05-29 21:19:11 +0000

    Equiv. of r89121 for 1.17wmf1
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@89136 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.17wmf1/extensions/LiquidThreads/classes/View.php
+++ 1.17wmf1/extensions/LiquidThreads/classes/View.php
@@ -1208,7 +1208,7 @@ class LqtView {
 		$wgOut->addExtensionStyle( "$wgLiquidThreadsExtensionPath/lqt.css?{$wgStyleVersion}" );
 
 		# Hack to make old LQT work with new WikiEditor, hopefully --catrope
-		$wgOut->addModules( array( 'ext.wikiEditor', 'ext.wikiEditor.toolbar.i18n', 'jquery.wikiEditor.toolbar', 'jquery.wikiEditor.dialogs', 'jquery.async', 'jquery.cookie', 'jquery.ui.dialog' ) );
+		$wgOut->addModules( array( 'ext.wikiEditor', 'ext.wikiEditor.toolbar.i18n', 'jquery.wikiEditor.toolbar', 'jquery.wikiEditor.dialogs', 'jquery.async', 'jquery.cookie', 'jquery.ui.dialog', 'jquery.ui.droppable' ) );
 	
 		self::$stylesAndScriptsDone = true;
 	}
