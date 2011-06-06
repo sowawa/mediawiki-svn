6bf507840457f7db8717d55ae25c24acca1127b7
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-06-06 11:10:23 +0000

    1.17wmf1: Copy WikimediaIncubator extension from trunk
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@89556 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ 1.17wmf1/extensions/WikimediaIncubator/SpecialRandomByTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Special page to go to a random page in your test wiki
+ * @file
+ */
+
+class SpecialRandomByTest extends RandomPage
+{
+	public function __construct() {
+		global $wgUser, $wmincPref, $wmincProjectSite;
+		if( IncubatorTest::isContentProject() ) {
+			$dbr = wfGetDB( DB_SLAVE );
+			$this->extra[] = 'page_title' .
+				$dbr->buildLike( IncubatorTest::displayPrefix() . '/', $dbr->anyString() );
+		} elseif($wgUser->getOption($wmincPref . '-project') == $wmincProjectSite['short'] ) {
+			// project or help namespace
+			$this->extra['page_namespace'] = array( 4, 12 );
+		}
+		parent::__construct( 'RandomByTest' );
+	}
+}
---
+++ 1.17wmf1/extensions/WikimediaIncubator/SpecialViewUserLang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Provides a special page to look up user language and test wiki
+ * This can be useful e.g. when a user has a problem with the preference system,
+ * so the test wiki/language information can be easily looked up
+ *
+ * Based on code from extension LookupUser made by Tim Starling
+ *
+ * @file
+ */
+
+class SpecialViewUserLang extends SpecialPage
+{
+	public function __construct() {
+		parent::__construct( 'ViewUserLang', 'viewuserlang' );
+	}
+
+	function getDescription() { return wfMsg( 'wminc-viewuserlang' ); }
+
+	/**
+	 * Show the special page
+	 * @param $subpage Mixed: parameter passed to the page or null
+	 */
+	public function execute( $subpage ) {
+		global $wgRequest, $wgUser;
+		
+
+		$this->setHeaders();
+
+		if ( !$wgUser->isAllowed( 'viewuserlang' ) ) {
+			$this->displayRestrictionError();
+			return;
+		}
+
+		$target = $wgRequest->getText( 'target', $subpage );
+
+		$this->showForm( $target );
+
+		if ( $target ) {
+			$this->showInfo( $target );
+		}
+	}
+
+	/**
+	 * Show the ViewUserLang form
+	 * @param $target Mixed: user whose language and test wiki we're about to look up
+	 */
+	function showForm( $target ) {
+		global $wgScript, $wgOut;
+
+		$wgOut->addHTML(
+			Xml::fieldset( wfMsgHtml( 'wminc-viewuserlang' ) ) .
+			Xml::openElement( 'form', array( 'method' => 'get', 'action' => $wgScript ) ) .
+			Html::hidden( 'title', $this->getTitle()->getPrefixedText() ) .
+			"<p>" .
+				Xml::inputLabel( wfMsgHtml( 'wminc-viewuserlang-user' ), 'target', 'viewuserlang-username', 40, $target ) .
+				' ' .
+				Xml::submitButton( wfMsgHtml( 'wminc-viewuserlang-go' ) ) .
+			"</p>" .
+			Xml::closeElement( 'form' ) .
+			Xml::closeElement( 'fieldset' )
+		);
+	}
+
+	/**
+	 * Retrieves and shows the user language and test wiki
+	 * @param $target Mixed: user whose language and test wiki we're looking up
+	 */
+	function showInfo( $target ) {
+		global $wgOut, $wmincPref, $wmincProjectSite;
+		$sk = $this->getSkin();
+		$user = User::newFromName( $target );
+		$langNames = Language::getLanguageNames();
+		if ( $user == null || $user->getId() == 0 ) {
+			// show error if a user with that name does not exist
+			$wgOut->addHTML( Xml::span( wfMsg( 'wminc-userdoesnotexist', $target ), 'error' ) );
+		} else {
+			$prefix = IncubatorTest::displayPrefix(
+				$user->getOption( $wmincPref . '-project' ),
+				$user->getOption( $wmincPref . '-code' )
+			);
+			if ( IncubatorTest::isContentProject() ) {
+				$testwiki = $sk->link( Title::newFromText( $prefix ) );
+			} elseif ( $prefix == $wmincProjectSite['short'] ) {
+				$testwiki = htmlspecialchars( $wmincProjectSite['name'] );
+			} else {
+				$testwiki = wfMsgHtml( 'wminc-testwiki-none' );
+			}
+			$name = $user->getName();
+			$wgOut->addHtml(
+				Xml::openElement( 'ul' ) .
+				'<li>' . wfMsgHtml( 'username' ) . ' ' .
+					$sk->userLink( $name, $name ) . $sk->userToolLinks( $name, $name ) . '</li>' .
+				'<li>' . wfMsgHtml( 'loginlanguagelabel', $langNames[$user->getOption( 'language' )] .
+					' (' . $user->getOption( 'language' ) . ')' ) . '</li>' .
+				'<li>' . wfMsgHtml( 'wminc-testwiki' ) . ' ' . $testwiki . '</li>' .
+				Xml::closeElement( 'ul' )
+			);
+		}
+	}
+}
